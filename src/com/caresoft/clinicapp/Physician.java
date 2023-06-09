adccd0b0aef86edcf8d0fd001bb42de7f852ddc4
Lucas Sousa <lucasns241@gmail.com>
2023-06-08 22:28:51 -0400

    first commit

---
+++ src/com/caresoft/clinicapp/Physician.java
@@ -0,0 +1,47 @@
+package com.caresoft.clinicapp;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Physician extends User implements HIPAACompliantUser {
+
+    // TO DO: Constructor that takes an ID
+	public Physician(Integer id) {
+		super(id);
+	}
+	
+	private ArrayList<String> patientNotes;
+
+    // TO DO: Implement HIPAACompliantUser!
+	public boolean assignPin (int pin) {
+		if(String.valueOf(pin).length() != 4) {
+			return false;
+		}
+		return true;
+	}
+	
+	public boolean accessAuthorized (Integer confirmedAuthID) {
+		if (this.id == confirmedAuthID) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+    public void newPatientNotes(String notes, String patientName, Date date) {
+        String report = String.format(
+            "Datetime Submitted: %s \n", date);
+        report += String.format("Reported By ID: %s\n", this.id);
+        report += String.format("Patient Name: %s\n", patientName);
+        report += String.format("Notes: %s \n", notes);
+        this.patientNotes.add(report);
+    }
+
+	public ArrayList<String> getPatientNotes() {
+		return patientNotes;
+	}
+
+	public void setPatientNotes(ArrayList<String> patientNotes) {
+		this.patientNotes = patientNotes;
+	}
+
+}
